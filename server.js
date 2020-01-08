9d41a9b5935e84697f93cd7388fc11865e3160b5
John Barreiros <jbarreiros2@gmail.com>
2020-01-08 00:00:58 -0500

    minor changes after testing on a non-dev server

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-process.title = "BingoApp";
+/**
+ * Bingo App
+ */
 
+const path = require("path");
 const express = require("express");
 const engine = require("ejs-blocks");
 const app = express();
 require("express-ws")(app);
-const port = 8080;
+
+const HTTP_HOSTNAME = process.env.HOST || "localhost";
+const HTTP_PORT = process.env.PORT || 8080;
+const IS_PROD = process.env.NODE_ENV === "production";
+
+// make it easier to kill the process; only really useful for development
+process.title = "BingoApp";
 
 // frontend
 app.set("views", "./src/views");
 app.engine("ejs", engine);
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 app.use("/", require("./src/routes/app"));
+
+if (!IS_PROD) {
+    app.use("/", express.static(path.join(__dirname, "/public")));
+}
 
 // websocket
 app.use("/", require("./src/routes/websocket"));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(HTTP_PORT, HTTP_HOSTNAME, () =>
+  console.log(`Listening on ${HTTP_HOSTNAME}:${HTTP_PORT}`)
+);
