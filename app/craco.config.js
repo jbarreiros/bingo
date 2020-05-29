37a4de1fa14b3990859c602d3a43502cd863536d
John Barreiros <johnbarreiros2@gmail.com>
2020-05-28 21:55:36 -0400

    make craco actually work

--- app/craco.config.js
+++ app/craco.config.js
@@ -3,14 +3,14 @@ module.exports = {
         postcss: {
             mode: "extends",
             loaderOptions: {
-                plugins: {
+                plugins: [
+                  require("postcss-env-function")({
                     // Allow usage of the "env(--css-var)" syntax, which is not official yet.
                     // Consider an "@media()" rule. In that context, "var(--my-var)" does
                     // not work. The "env(--my-bar)" syntax is being proposed as an alternative.
-                    "postcss-env-function": {
-                        importFrom: "./src/postcss-environment-variables.js"
-                    }
-                }
+                    importFrom: "./src/postcss-environment-variables.js"
+                  })
+                ]
             }
         }
     }
